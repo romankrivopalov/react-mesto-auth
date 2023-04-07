001fa117f9615a8f90ac56b85032834bdb84fc60
laptop <the.red.lemon@yandex.ru>
2023-04-07 20:48:46 +0300

    feat: add initials routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import Header from './Header';
 import Main from './Main';
@@ -11,7 +12,8 @@ import ConfirmPopup from './ConfirmPopup';
 import api from '../utils/api';
 
 function App() {
-  const [ currentUser, setCurrentUser ] = useState({}),
+  const [ loggedIn, setLoggedIn ] = useState(false),
+        [ currentUser, setCurrentUser ] = useState({}),
         [ isLoading, setIsLoading ] = useState(false),
         [ isEditAvatarPopupOpen, setIsEditAvatarPopupOpen ] = useState(false),
         [ isEditProfilePopupOpen, setIsEditProfilePopupOpen ] = useState(false),
@@ -105,56 +107,68 @@ function App() {
   }
 
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <div className="App">
+    <BrowserRouter>
+      <CurrentUserContext.Provider value={currentUser}>
+        <div className="App">
 
-        <Header />
-        <Main
-          onEditAvatar={setIsEditAvatarPopupOpen}
-          onEditProfile={setIsEditProfilePopupOpen}
-          onAddPlace={setIsAddPlacePopupOpen}
-          cards={cards}
-          onCardClick={handleCardClick}
-          onCardLike={handleCardLike}
-          onCardDelete={handleCardDelete}
+          <Header />
+
+          <main>
+            <Routes>
+              <Route path='/' element={loggedIn
+                ? <Main
+                  onEditAvatar={setIsEditAvatarPopupOpen}
+                  onEditProfile={setIsEditProfilePopupOpen}
+                  onAddPlace={setIsAddPlacePopupOpen}
+                  cards={cards}
+                  onCardClick={handleCardClick}
+                  onCardLike={handleCardLike}
+                  onCardDelete={handleCardDelete}/>
+                : <Navigate to="/login" replace />
+              } />
+              <Route path='/sign-in' />
+              <Route path='/sign-up' />
+            </Routes>
+          </main>
+
+          <Footer />
+
+          <EditAvatarPopup
+            isOpen={isEditAvatarPopupOpen}
+            onClose={closeAllPopups}
+            onUpdateAvatar={handleUpdateAvatar}
+            isLoading={isLoading}
           />
-        <Footer />
 
-        <EditAvatarPopup
-          isOpen={isEditAvatarPopupOpen}
-          onClose={closeAllPopups}
-          onUpdateAvatar={handleUpdateAvatar}
-          isLoading={isLoading}
-        />
+          <EditProfilePopup
+            isOpen={isEditProfilePopupOpen}
+            onClose={closeAllPopups}
+            onUpdateUser={handleUpdateUser}
+            isLoading={isLoading}
+          />
 
-        <EditProfilePopup
-          isOpen={isEditProfilePopupOpen}
-          onClose={closeAllPopups}
-          onUpdateUser={handleUpdateUser}
-          isLoading={isLoading}
-        />
+          <AddPlacePopup
+            isOpen={isAddPlacePopupOpen}
+            onClose={closeAllPopups}
+            onAddPlace={handleAddPlaceSubmit}
+            isLoading={isLoading}
+          />
 
-        <AddPlacePopup
-          isOpen={isAddPlacePopupOpen}
-          onClose={closeAllPopups}
-          onAddPlace={handleAddPlaceSubmit}
-          isLoading={isLoading}
-        />
+          <ImagePopup
+            card={selectedCard}
+            onClose={closeAllPopups}
+          />
 
-        <ImagePopup
-          card={selectedCard}
-          onClose={closeAllPopups}
-        />
+          <ConfirmPopup
+            isOpen={!!cardToDelete}
+            onClose={closeAllPopups}
+            onConfirm={handleConfirmBeforeDelete}
+            isLoading={isLoading}
+          />
 
-        <ConfirmPopup
-          isOpen={!!cardToDelete}
-          onClose={closeAllPopups}
-          onConfirm={handleConfirmBeforeDelete}
-          isLoading={isLoading}
-        />
-
-      </div>
-    </CurrentUserContext.Provider>
+        </div>
+      </CurrentUserContext.Provider>
+    </BrowserRouter>
   );
 }
 
