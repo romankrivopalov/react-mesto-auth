d1381d4cd441c4c132031131caf8bb7b453dc5ba
Home <home@gmail.com>
2023-04-10 19:22:27 +0300

    fefactor: change modal closure

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -4,7 +4,13 @@ import iconError from '../images/icon-error.svg';
 function InfoTooltip({ isOpen, onClose }) {
 
   return (
-    <section className={`info-tooltip ${isOpen ? 'info-tooltip_opened' : ''}`}>
+    <section
+      className={`info-tooltip ${isOpen ? 'info-tooltip_opened' : ''}`}
+      onClick={({ target }) => {
+        if (target.classList.contains('info-tooltip_opened') || target.classList.contains('info-tooltip__close')) {
+          onClose();
+        }
+      }}>
       <div className="info-tooltip__container">
         <button type="button" onClick={onClose} className="info-tooltip__close" />
         <img src={iconDone} className="info-tooltip__img" alt=""></img>
