70913826c236a9cac938f860e2424571fb71e1ae
laptop <the.red.lemon@yandex.ru>
2023-04-12 21:25:20 +0300

    refactor: change routes to useLocation hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,51 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import logoWhite from '../images/logo-white.svg'
 
 function Header({ userEmail, onSignOut, isOpenBurger, onToggleBurger }) {
-  return (
-    <Routes>
+  const { pathname } = useLocation();
 
-      <Route path='/' element={
-        <header className="header container">
+  return (
+      <header className="header container">
+        { pathname === '/' &&
           <section className={`header__aside ${isOpenBurger ? 'header__aside_opened' : ''}`}>
             <p className='header__email'>{userEmail}</p>
             <button onClick={onSignOut} className='header__btn'>
               Выйти
             </button>
           </section>
+        }
 
-          <div className='header__columns'>
-            <img src={logoWhite} alt=" Логотип сервиса Место." className="header__logo" />
+        <div className='header__columns'>
+          <img src={logoWhite} alt=" Логотип сервиса Место." className="header__logo" />
 
+          { pathname === '/' &&
             <div className='header__container'>
               <button className='header__burger' onClick={onToggleBurger}>
                 <div className={`header__burger-inner ${isOpenBurger ? 'header__burger-inner_active' : ''}`}></div>
               </button>
               <div className='header__wrapper'>
                 <p className='header__email'>{userEmail}</p>
-              <button onClick={onSignOut} className='header__btn'>
-                Выйти
-              </button>
-           </div>
-         </div>
-          </div>
-        </header>
-      }/>
+                <button onClick={onSignOut} className='header__btn'>
+                  Выйти
+                </button>
+              </div>
+            </div>
+          }
 
-      <Route path='/signin' element={
-        <header className="header container">
-          <div className='header__columns'>
-            <img src={logoWhite} alt=" Логотип сервиса Место." className="header__logo" />
+          { pathname === '/signin' &&
             <Link to='/signup' className='header__link'>
               Регистрация
             </Link>
-          </div>
-        </header>
-      }/>
+          }
 
-      <Route path='/signup' element={
-        <header className="header container">
-          <div className='header__columns'>
-            <img src={logoWhite} alt=" Логотип сервиса Место." className="header__logo" />
+          { pathname === '/signup' &&
             <Link to='/signin' className='header__link'>
               Войти
             </Link>
-          </div>
-        </header>
-      }/>
-
-    </Routes>
+          }
+        </div>
+      </header>
   )
 }
 
