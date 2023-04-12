3f96723f724ffcaaa01da630797ff93d54a45d87
laptop <the.red.lemon@yandex.ru>
2023-04-12 21:15:00 +0300

    refactor: delete commented line

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,51 +55,6 @@ function Header({ userEmail, onSignOut, isOpenBurger, onToggleBurger }) {
       }/>
 
     </Routes>
-
-
-    // <header className="header container">
-    //   <section className={`header__aside ${isOpenBurger ? 'header__aside_opened' : ''}`}>
-    //     <p className='header__email'>{userEmail}</p>
-    //     <button onClick={onSignOut} className='header__btn'>
-    //       Выйти
-    //     </button>
-    //   </section>
-
-    //   <div className='header__columns'>
-    //     <Link to='/'>
-    //       <img src={logoWhite} alt=" Логотип сервиса Место." className="header__logo" />
-    //     </Link>
-
-    //     <Routes>
-    //       <Route path='/' element={
-    //         <div className='header__container'>
-    //           <button className='header__burger' onClick={onToggleBurger}>
-    //             <div className='header__burger-inner'></div>
-    //           </button>
-    //           <div className='header__wrapper'>
-    //             <p className='header__email'>{userEmail}</p>
-    //             <button onClick={onSignOut} className='header__btn'>
-    //               Выйти
-    //             </button>
-    //           </div>
-    //         </div>
-    //       } />
-
-    //       <Route path='/signin' element={
-    //         <Link to='/signup' className='header__link'>
-    //           Регистрация
-    //         </Link>
-    //       } />
-
-    //       <Route path='/signup' element={
-    //         <Link to='/signin' className='header__link'>
-    //           Войти
-    //         </Link>
-    //       } />
-
-    //     </Routes>
-    //   </div>
-    // </header>
   )
 }
 
